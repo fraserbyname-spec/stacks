e964e37ff8945e80c164ddff603d39a72d6975f0
Fraser Clark <fraserbyname@gmail.com>
2026-05-05 11:38:03 +1200

    changed URL to www again to avoid the weird share card

--- app/page.tsx
+++ app/page.tsx
@@ -210,7 +210,7 @@ export default function Home() {
 
   const formatBalance = (n: number) => `$${n.toLocaleString()}`
 
-  const shareText = `I just STACKED:\n${formatBalance(lastRunBalance)}\nNailed ${lastRunPicks} pick${lastRunPicks !== 1 ? 's' : ''} 💸\n\nhttps://stacksgame.app`
+  const shareText = `I just STACKED:\n${formatBalance(lastRunBalance)}\nNailed ${lastRunPicks} pick${lastRunPicks !== 1 ? 's' : ''} 💸\n\nwww.stacksgame.app`
 
   const handleShare = () => {
     if (navigator.share) {
